324e8df57f215ab41343ae82d08ba680dbb9ddf2
John Adams Jr <john.c.adams32@gmail.com>
2022-01-09 01:02:17 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# TODO: create scoreboard class
+# TODO: create pong ball class
+# TODO: create paddle class
+# TODO: update the scoreboard when a player scores
+# TODO: reset the pong ball if the players miss the ball
+# TODO: Create game play field using screen and turtles
