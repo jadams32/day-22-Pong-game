aa7e000e367f32f9aff4aa9a1c34ddda3707460e
John Adams Jr <john.c.adams32@gmail.com>
2022-01-10 21:15:56 -0700

    animations turned off

--- main.py
+++ main.py
@@ -4,3 +4,38 @@
 # TODO: update the scoreboard when a player scores
 # TODO: reset the pong ball if the players miss the ball
 # TODO: Create game play field using screen and turtles
+
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+
+
+net = Turtle()
+net.hideturtle()
+net.color("white")
+net.pencolor("white")
+net.pensize(5)
+net.setheading(90)
+net.hideturtle()
+net.penup()
+net.goto(x=0, y=-600)
+net.pendown()
+
+
+for num in range(60):
+    net.forward(10)
+    net.penup()
+    net.forward(10)
+    net.pendown()
+
+
+
+
+
+
+screen.exitonclick()
